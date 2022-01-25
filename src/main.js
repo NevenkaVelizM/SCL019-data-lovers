aabc3a27056dcca1385905c162dd4818477d3de0
unknown <nvelizm@gmail.com>
2022-01-25 09:19:03 -0300

    primer commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let character = data.results;
+
+let table = document.getElementById("table");
+
+window.addEventListener("load", addAllCharacters(character));
+
+function addAllCharacters(character) {
+
+  table.innerHTML = "";
+  
+  for (let i = 0; i < character.length; i++) {
+
+    let tarjeta = document.createElement("div");
+    let image = document.createElement("img");
+    let name = document.createElement("p");
+    
+    tarjeta.setAttribute("class", "cell"); 
+    tarjeta.setAttribute("id", "card"); 
+    image.setAttribute("src", character[i].image);
+    image.setAttribute("class", "image");
+    name.innerHTML = character[i].name;
+    
+    
+    table.appendChild(tarjeta);
+    tarjeta.appendChild(image);
+    tarjeta.appendChild(name);
+    
+  }
+}
